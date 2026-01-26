118ae63f3ae9e01ca06c3be03cb296accb4f8830
FEDERICOMAR79 <federmarquezcalle@gmail.com>
2026-01-26 06:57:09 -0500

    Delete texto/contador directory

--- texto/contador/app.js
+++
@@ -1,6 +0,0 @@
-const texto = document.getElementById("texto");
-const contador = document.getElementById("contador");
-
-texto.addEventListener("input", () => {
-  contador.textContent = texto.value.length;
-});
